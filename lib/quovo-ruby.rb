f32ab0ce258e5408f6b478e910e3d9f55f9205c0
Matt De Leon <smidwap@gmail.com>
2019-01-14 18:08:28 -0500

    Forget about byebug for now

--- lib/quovo-ruby.rb
+++ lib/quovo-ruby.rb
@@ -3,7 +3,7 @@
 require 'active_support/all'
 
 # TODO: pass in environment variable or something so these only load when passed...or figure out a better way
-require 'byebug'
+# require 'byebug'
 require 'awesome_print'
 
 require 'quovo/base'
